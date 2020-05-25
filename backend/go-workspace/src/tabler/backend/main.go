b4387af755a7ab5c401a68cf13d23d59867efae6
Rafael Vilas Boas de Carvalho <az.carta@gmail.com>
2020-05-25 18:49:14 -0300

    criado rota homePage e createRoom. XD

--- backend/go-workspace/src/tabler/backend/main.go
+++ backend/go-workspace/src/tabler/backend/main.go
@@ -6,6 +6,7 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http" //TO HANDLE HTTP REQUEST
 
@@ -48,10 +49,10 @@ func main() {
 	//THE FIRST PARAMETER IS THE NAME OF THE ROUT "/something/whatever", THE SECOND, IS THE FUNCTION IT CALLS.
 	//THEN WE USE .Methods(GET OR POST OR DELETE OR WHATEVER) TO SPECIFY THE METHOD WE ARE USING IN THIS ROUT
 	r.HandleFunc("/api/homePage", homePage).Methods("GET")
-	/*r.HandleFunc("/api/searchRooms/{info}", searchRooms).Methods("GET")
+	//r.HandleFunc("/api/searchRooms/{info}", searchRooms).Methods("GET")
 	r.HandleFunc("/api/createRoom", createRoom).Methods("POST")
-	r.HandleFunc("/api/updateRoom/{id}", updateRoom).Methods("PUT")
-	r.HandleFunc("/api/deleteRoom/{id}", deleteRoom).Methods("DELETE")*/
+	//r.HandleFunc("/api/updateRoom/{id}", updateRoom).Methods("PUT")
+	//r.HandleFunc("/api/deleteRoom/{id}", deleteRoom).Methods("DELETE")
 
 	log.Println("Server Online!")
 	//SETS THE SERVER UP AT PORT 3035. YOU CAN USE WHATEVER PORT YOU WANT
@@ -101,7 +102,32 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 //createRoom FUNCTION
 func createRoom(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+	stmtIns, err := db.Prepare("INSERT INTO mesa(ADM_MESA, TITULO_MESA, DESC_MESA, QTDEJOG_MESA, FORMA_MESA, STATUS_MESA) VALUES (?,?,?,?,?,?) ")
+	//stmtIns, err := db.Prepare("INSERT INTO mesa(TITULO_MESA, DESC_MESA) VALUES (?,?)")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	keyVal := make(map[string]string)
+	json.Unmarshal(body, &keyVal)
+	admMesa := keyVal["ADM_MESA"]
+	tituloMesa := keyVal["TITULO_MESA"]
+	descMesa := keyVal["DESC_MESA"]
+	qtdejogMesa := keyVal["QTDEJOG_MESA"]
+	formaMesa := keyVal["FORMA_MESA"]
+	statusMesa := keyVal["STATUS_MESA"]
+
+	_, err = stmtIns.Exec(admMesa, tituloMesa, descMesa, qtdejogMesa, formaMesa, statusMesa)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Fprintf(w, "Mesa criada com sucesso!")
 }
 
 //searchRooms FUNCTION
