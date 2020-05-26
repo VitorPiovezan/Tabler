7fceda32332d504e4f88fc2f0ca1337b66c0a96f
Rafael Vilas Boas de Carvalho <az.carta@gmail.com>
2020-05-26 01:41:57 -0300

    commit insano da madrugada. TODAS AS ROTAS TAO FUNCIONANDO, BO BOTA ESSE NEGÓCIO PRA RODA. Preciso dormir, boa noite Vitu <3... zzzzzzzzzzzzzZZZZZzzzzzzzzzz

--- backend/go-workspace/src/tabler/backend/main.go
+++ backend/go-workspace/src/tabler/backend/main.go
@@ -8,7 +8,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"net/http" //TO HANDLE HTTP REQUEST
+	"net/http" //TO HANDLE HTTP REQUESTS
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
@@ -29,7 +29,6 @@ type Room struct {
 var db *sql.DB
 var err error
 
-//LIKE IN C, IT ALWAYS NEED A MAIN FUNCTION TO START THINGS OFF
 func main() {
 
 	db, err = sql.Open("mysql", "user_tester:123456@tcp(127.0.0.1:3000)/tabler_db")
@@ -49,17 +48,17 @@ func main() {
 	r.HandleFunc("/api/homePage", homePage).Methods("GET")
 	r.HandleFunc("/api/searchRooms/{tituloMesa}", searchRooms).Methods("GET")
 	r.HandleFunc("/api/createRoom", createRoom).Methods("POST")
-	//r.HandleFunc("/api/updateRoom/{id}", updateRoom).Methods("PUT")
-	r.HandleFunc("/api/deleteRoom/{id}", deleteRoom).Methods("DELETE")
+	r.HandleFunc("/api/joinRoom", joinRoom).Methods("POST")
+	r.HandleFunc("/api/updateRoom", updateRoom).Methods("PUT")
+	r.HandleFunc("/api/deleteRoom", deleteRoom).Methods("DELETE")
 	//-----------------------------------------------------------------------
 
 	//----------------------USER ROUTES--------------------------------------
 	r.HandleFunc("/api/createUser", createUser).Methods("POST")
-	//r.HandleFunc("/api/updateProfile/{id}", updateProfile).Methods("DELETE")
+	r.HandleFunc("/api/updateProfile", updateProfile).Methods("PUT")
 	//-----------------------------------------------------------------------
 
 	log.Println("Server Online!")
-
 	log.Fatal(http.ListenAndServe(":8000", r))
 
 }
@@ -94,6 +93,32 @@ func createUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func updateProfile(w http.ResponseWriter, r *http.Request) {
+
+	stmtIns, err := db.Prepare("UPDATE usuario SET NOME_USUAR = ? , APELIDO_USUAR = ? , SENHA_USUAR = ? , EMAIL_USUAR = ? , AVATAR_USUAR = ? WHERE ID_USUAR = ?")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	keyVal := make(map[string]string)
+	json.Unmarshal(body, &keyVal)
+	nomeUsuar := keyVal["NOME_USUAR"]
+	apelidoUsuar := keyVal["APELIDO_USUAR"]
+	senhaUsuar := keyVal["SENHA_USUAR"]
+	emailUsuar := keyVal["EMAIL_USUAR"]
+	avatarUsuar := keyVal["AVATAR_USUAR"]
+	idUsuar := keyVal["ID_USUAR"]
+
+	_, err = stmtIns.Exec(nomeUsuar, apelidoUsuar, senhaUsuar, emailUsuar, avatarUsuar, idUsuar)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Fprintf(w, "Dados do usuario alterados com sucesso!")
 }
 
 //--------------------------------------------------------------------------
@@ -129,8 +154,9 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 //createRoom FUNCTION
 func createRoom(w http.ResponseWriter, r *http.Request) {
+
 	stmtIns, err := db.Prepare("INSERT INTO mesa(ADM_MESA, TITULO_MESA, DESC_MESA, QTDEJOG_MESA, FORMA_MESA, STATUS_MESA) VALUES (?,?,?,?,?,?) ")
-	//stmtIns, err := db.Prepare("INSERT INTO mesa(TITULO_MESA, DESC_MESA) VALUES (?,?)")
+
 	if err != nil {
 		panic(err.Error())
 	}
@@ -189,17 +215,81 @@ func searchRooms(w http.ResponseWriter, r *http.Request) {
 
 //joinRoom FUNCTION
 func joinRoom(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+
+	stmtIns, err := db.Prepare("INSERT INTO mesa_jogadores(ID_MESA, ID_USUAR, MESTRE_JOGA, FICHA_JOGA) VALUES (?,?,?,?)")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	keyVal := make(map[string]string)
+	json.Unmarshal(body, &keyVal)
+	idMesa := keyVal["ID_MESA"]
+	idUsuar := keyVal["ID_USUAR"]
+	mestreJoga := keyVal["MESTRE_JOGA"]
+	fichaJoga := keyVal["FICHA_JOGA"]
+
+	//CHECK IF THERE'S ALREADY A DM AT THE TABLE
+	var isThereDm int
+	_ = db.QueryRow("SELECT COUNT(*) FROM mesa_jogadores WHERE ID_MESA = ? AND MESTRE_JOGA = 1", idMesa).Scan(&isThereDm)
+
+	if err != nil {
+
+		panic(err.Error())
+
+	}
+
+	if isThereDm != 0 && mestreJoga == "1" {
+
+		fmt.Fprintf(w, "Já existe mestre nesta mesa!")
+
+	} else { //IF THERE'S NO DM, INSERT THE PLAYER IN THE ROOM
+
+		_, err = stmtIns.Exec(idMesa, idUsuar, mestreJoga, fichaJoga)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		fmt.Fprintf(w, "Jogador inserido na mesa!")
+	}
+
 }
 
 //updateRoom FUNCTION
 func updateRoom(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+
+	stmtIns, err := db.Prepare("UPDATE mesa SET TITULO_MESA = ? , DESC_MESA = ? , QTDEJOG_MESA = ? , FORMA_MESA = ? WHERE ID_MESA = ?")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	keyVal := make(map[string]string)
+	json.Unmarshal(body, &keyVal)
+	idMesa := keyVal["ID_MESA"]
+	tituloMesa := keyVal["TITULO_MESA"]
+	descMesa := keyVal["DESC_MESA"]
+	qtdejogMesa := keyVal["QTDEJOG_MESA"]
+	formaMesa := keyVal["FORMA_MESA"]
+
+	_, err = stmtIns.Exec(tituloMesa, descMesa, qtdejogMesa, formaMesa, idMesa)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Fprintf(w, "Dados da mesa alterados com sucesso!")
 }
 
 //deleteRoom FUNCTION
 func deleteRoom(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
 
 	stmt, err := db.Prepare("DELETE FROM mesa WHERE ID_MESA = ?")
 
@@ -207,12 +297,21 @@ func deleteRoom(w http.ResponseWriter, r *http.Request) {
 		panic(err.Error())
 	}
 
-	_, err = stmt.Exec(params["id"])
+	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		panic(err.Error())
 	}
 
-	fmt.Fprintf(w, "Mesa com o ID = %s foi deletada", params["id"])
+	keyVal := make(map[string]string)
+	json.Unmarshal(body, &keyVal)
+	idMesa := keyVal["ID_MESA"]
+
+	_, err = stmt.Exec(idMesa)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Fprintf(w, "Mesa com o ID = %s foi deletada", idMesa)
 }
 
 //-------------------------------------------------------
